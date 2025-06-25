7e72c7df1fd167c6628d608871db065695e0f5f5
AY1048576 <github@me.averyyanowitz.com>
2025-06-25 13:50:57 -0400

    initial commit

--- src/main/java/com/slc/tools/Benchmarker.java
+++ src/main/java/com/slc/tools/Benchmarker.java
@@ -1,45 +1,54 @@
 package com.slc.tools;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 public class Benchmarker {
     private volatile int _completedCalls;
-    private final Callable<Object> _BENCHMARK_FUNCTION;
-    
-    public static void main(String[] args) {
-        Benchmarker benchmarker = new Benchmarker(() -> null);
-        benchmarker.test(1000, 2);
-    }
-
-
-    public Benchmarker(Callable<Object> functionToBenchmark) {
-        _completedCalls = 0;
-        _BENCHMARK_FUNCTION = () -> {
-            functionToBenchmark.call();
-            _completedCalls++;
-            return null;
-        };
-    }
+    private final Runnable _BENCHMARK_FUNCTION;
 
     public Benchmarker(Runnable functionToBenchmark) {
         _completedCalls = 0;
-        _BENCHMARK_FUNCTION = Executors.callable(() -> {
-            while (true) {
-                functionToBenchmark.run();
-                _completedCalls++;
-            }
-        }, null);
+        _BENCHMARK_FUNCTION = () -> {
+            functionToBenchmark.run();
+            _completedCalls++;
+        };
     }
 
-    public void test(int durationInMillis, int iterationsToRun) {
-        final ExecutorService execService = Executors.newSingleThreadExecutor();
+    public static void main(String[] args) {
+        Benchmarker benchmarker = new Benchmarker(() -> {
+            try {
+                Thread.sleep(10);
+            } catch (Exception e) {
+                System.out.println("Interrupted!");
+            }
+        });
+        benchmarker.test();
+    }
+
+    public void test() {
+        test(80, Duration.ofSeconds(100));
+    }
+
+    public void test(int numberOfLoops, Duration maxDuration) {
+        long maxNanoTime = maxDuration.toNanos();
+        int outerLoops, innerLoops;
+        if (numberOfLoops < 20) {
+            outerLoops = 1;
+            innerLoops = numberOfLoops;
+        } else {
+            outerLoops = 10;
+            innerLoops = numberOfLoops / 10;
+        }
+
         long startTime = System.nanoTime();
-        for (int i = 0; i < iterationsToRun; i++) {
-            _oneIter(durationInMillis, execService);
+        for (int i = 0; i < outerLoops; i++) {
+            if ((System.nanoTime() - startTime) < maxNanoTime) {
+                for (int j = 0; j < innerLoops; j++) {
+                    _BENCHMARK_FUNCTION.run();
+                }
+            } else {
+                break;
+            }
         }
         float elapsedTimeMillis = (System.nanoTime() - startTime) / 1_000_000.0f;
         float callsPerSecond = (_completedCalls / elapsedTimeMillis) * 1000;
@@ -47,33 +56,14 @@ public class Benchmarker {
         StringBuilder summaryMessage = new StringBuilder();
         summaryMessage.append("Executed ");
         summaryMessage.append(callsPerSecond);
-        summaryMessage.append(" times per second (total runtime ");
+        summaryMessage.append(" times per second (total: ");
+        summaryMessage.append(_completedCalls);
+        summaryMessage.append(" calls in ");
         summaryMessage.append(elapsedTimeMillis);
         summaryMessage.append(" ms)");
 
         System.out.println(summaryMessage.toString());
 
-        _expeditedShutdown(execService);
-    }
-
-    private void _oneIter(int durationInMillis, ExecutorService execService) {
-        try {
-            final Future<Object> f = execService.submit(_BENCHMARK_FUNCTION);
-            f.get(durationInMillis, TimeUnit.MILLISECONDS);
-        } catch (final Exception e) {
-            return;
-        }
-    }
-
-    private static void _expeditedShutdown(ExecutorService executorService) {
-        executorService.shutdown();
-        try {
-            if (!executorService.awaitTermination(800, TimeUnit.MILLISECONDS)) {
-                executorService.shutdownNow();
-            } 
-        } catch (InterruptedException e) {
-            executorService.shutdownNow();
-        }
     }
 
 }
